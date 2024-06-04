e783392a1d1d36415eee3cb8e4b5c0ba308057c2
Gabriela Ortiz <73257443+Gabriela-Ortiz@users.noreply.github.com>
2024-06-04 12:02:47 -0600

    Add files via upload

---
+++ test.java
@@ -0,0 +1,19 @@
+package interview;
+
+public class test {
+	
+	String[] romNum = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C"};
+	int[] intNum = {1,4,5,9,10,40,50,90,100};
+	
+	String input = "IV";
+
+	
+
+	for(i=0; i<=romNum.length; i++) {
+		if() {
+			//
+		}
+	}
+	
+
+}
